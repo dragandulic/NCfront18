d54ce5cecbc06291ff83db3430cbe6bffece7066
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-12-16 00:37:06 +0100

    second commit

--- NCfront/src/components/About.vue
+++ NCfront/src/components/About.vue
@@ -6,7 +6,7 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
    
         <h1>Hello {{ their_name }}, this is an about page</h1>
-   
+        <p>111111</p>
     </div>
 
 </template>
